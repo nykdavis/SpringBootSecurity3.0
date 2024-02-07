75655bdef1a4ae8e62d7d4b6bfd621a565aec209
dvsnayak <yello.davis@gmail.com>
2024-02-07 22:56:10 +0530

    Initial project setup for spring security 3.0

---
+++ spring-security/src/main/java/com/example/spring/security/dto/Product.java
@@ -0,0 +1,19 @@
+package com.example.spring.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+	
+	private int productId;
+	private String name;
+	private int qty;
+	private double price;
+
+}
---
+++ spring-security/src/main/java/com/example/spring/security/service/ProductService.java
@@ -0,0 +1,34 @@
+package com.example.spring.security.service;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.springframework.stereotype.Service;
+
+import com.example.spring.security.dto.Product;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class ProductService {
+	
+	List<Product> productList = null;
+	
+	@PostConstruct
+	public void loadProductFromDB() {
+		productList = IntStream.rangeClosed(1, 100)
+				.mapToObj(i -> Product.builder()
+						.productId(i)
+						.name("Avatar"+i)
+						.qty(new Random().nextInt(10))
+						.price(new Random().nextInt(5000)).build()
+						).collect(Collectors.toList());
+	}
+	
+	public List<Product> getProducts(){
+		return productList;
+	}
+
+}
